e960a8a1af28c1f71f62ee0edaa4123d393070fd
zeeker <zeekerpro@gmail.com>
2023-08-27 21:11:40 +0800

    feature:   1. change logo   2. use while to set stream volume to zero

--- app/src/main/java/com/noob/shutup/MainService.kt
+++ app/src/main/java/com/noob/shutup/MainService.kt
@@ -4,22 +4,25 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.os.Build
 import android.os.IBinder
 import android.provider.Settings
-import androidx.core.content.getSystemService
 
 class MainService : Service() {
 
-    var preMusicVolume = 5
-    var preSystemVolume = 5
-    var preNotificationVolume  = 6
-
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
 
+        val sharedPreferences = this.getSharedPreferences("shutup", Context.MODE_PRIVATE)
+        var preMusicVolume = sharedPreferences.getInt("preMusicVolume", 5)
+        var preRingVolume = sharedPreferences.getInt("preRingVolume", 5)
+        var preNotificationVolume  = sharedPreferences.getInt("preNotificationVolume", 5)
+
         val notificationManager = getSystemService(NotificationManager::class.java)
+
+        val editor = sharedPreferences.edit()
 
         // check permission
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !notificationManager.isNotificationPolicyAccessGranted) {
@@ -28,27 +31,52 @@ class MainService : Service() {
             startActivity(intent)
         }
 
-        val audioManager = getSystemService(AudioManager::class.java)
+        //val audioManager = getSystemService(AudioManager::class.java)
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
         if (audioManager.ringerMode == AudioManager.RINGER_MODE_NORMAL){
             preMusicVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+            preRingVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING)
             preNotificationVolume = audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION)
+
+            // store current volume of system
+            editor.putInt("preMusicVolume", preMusicVolume)
+            editor.putInt("preRingVolume", preRingVolume)
+            editor.putInt("preNotificationVolume", preNotificationVolume)
+            editor.apply()
+
             // enable incoming call vibration mode
             audioManager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
             // set media volume to zero
             audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_SHOW_UI)
+
             audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, 0, AudioManager.FLAG_SHOW_UI)
             // mute the notification and system sound
             audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, AudioManager.FLAG_SHOW_UI )
+
+            // test
+            while (audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) > 0) {
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI)
+            }
 
             println("muted")
             println(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
             println(audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION))
         }else{
+            editor.putInt("preMusicVolume", preMusicVolume)
+            editor.putInt("preRingVolume", preRingVolume)
+            editor.putInt("preNotificationVolume", preNotificationVolume)
+
             audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
             audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, preMusicVolume, AudioManager.FLAG_SHOW_UI )
-            audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, preSystemVolume, AudioManager.FLAG_SHOW_UI )
+            audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, preRingVolume, AudioManager.FLAG_SHOW_UI )
             audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, preNotificationVolume, AudioManager.FLAG_SHOW_UI )
+
+            // test
+            while (audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) < preMusicVolume) {
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, 0)
+            }
+
             println("restored")
             println(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
             println(audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION))
